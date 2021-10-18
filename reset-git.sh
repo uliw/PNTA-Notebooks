39abd5adcef59bd32ea6a293cdb5ddc6b9f7b675
uliw <uli.wortmann@utoronto.ca>
2021-10-18 16:56:08 -0400

    changed branch name

--- reset-git.sh
+++ reset-git.sh
@@ -4,7 +4,7 @@ git pull
 git reset --hard HEAD
 
 # Update Tracked Notebooks to Latest
-git fetch; git reset --hard origin/master
+git fetch; git reset --hard origin/main
 
 # Nuke all Changes
-git clean -qfdx; git fetch; git reset --hard origin/master
+git clean -qfdx; git fetch; git reset --hard origin/main
