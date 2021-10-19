5181ab3578b3dbafe41b0b764048b84499532f67
uliw <uli.wortmann@utoronto.ca>
2021-10-19 08:43:28 -0400

    auto deploy

--- reset_git.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-subprocess.run(["git", "pull"])
-subprocess.run("git reset --hard HEAD".split())
-subprocess.run("git fetch; git reset --hard origin/main".split())
-subprocess.run("git clean -qfdx; git fetch; git reset --hard origin/main".split())
